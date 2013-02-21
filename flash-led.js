47e7b42ca27d5978029f936d2e804716e548ba28
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2013-02-21 00:02:53 +0000

    Remove unused code and libs

--- flash-led.js
+++ flash-led.js
@@ -1,12 +1,8 @@
-
+/*
+  Use the pi-gpio library and Q's promises to 
+  implement flashing LED
+*/
 var things = require("./lib/things");
-
-// var led = new things.LED(7);
-// led.ready()
-//    .then( function () { return led.on()  })
-//    .then( function () { return things.wait(2000); })
-//    .then( function () { return led.off() })
-//    .then( function () { console.log('done'); });
 
 var led = new things.LED(7);
 led.ready()
@@ -19,14 +15,3 @@ function flash() {
      .then( function () { return things.wait(1000); } )
      .then( function () { console.log('done'); flash(); });
 }
-
-/*
-var gpio  = require("pi-gpio");
-gpio.open(7, "output", function(err) {     // Open pin 16 for output
-    gpio.write(7, 0, function() {          // Set pin 16 high (1)
-        gpio.close(7);                     // Close pin 16
-    });
-});
-*/
-
-//setInterval(function () { console.log('tick'); }, 1000)
